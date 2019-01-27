0f5d08b8542027efb17fb8e37d0194728ef676fb
rigo_sarmiento <rigo.sarmiento@pm..me>
2019-01-27 11:57:19 +0800

    Initial commit

---
+++ src/main/java/rigor/io/irent/IrentApplication.java
@@ -0,0 +1,14 @@
+package rigor.io.irent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IrentApplication.class, args);
+  }
+
+}
+
---
+++ src/main/java/rigor/io/irent/user/User.java
@@ -0,0 +1,15 @@
+package rigor.io.irent.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+
+
+
+}
